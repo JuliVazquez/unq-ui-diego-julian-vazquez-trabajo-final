a102dfc7407b426efdfe2e5d461e3779c076bb11
JuliVazquez <julivazquez09@gmail.com>
2023-07-05 20:53:32 -0300

    agrego una IA para jugar y logica del juego

--- src/components/Molecules/PlayerMenu/PlayerMenu.js
+++ src/components/Molecules/PlayerMenu/PlayerMenu.js
@@ -1,24 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./PlayerMenu.css";
 import GameButton from "../../Atoms/GameButton/GameButton";
-import piedraImg from "../../../assets/images/piedra.jpg"
+import piedraImg from "../../../assets/images/piedra.jpg";
 import papelImg from "../../../assets/images/papel.jpg";
 import tijeraImg from "../../../assets/images/tijera.jpg";
 import lagartoImg from "../../../assets/images/lagarto.jpg";
 import spockImg from "../../../assets/images/spock.jpg";
+import backing from "../../../assets/images/backing-card.jpg";
 
 const PlayerMenu = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedPlayerOption, setSelectedPlayerOption] = useState(backing);
+  const [selectedIAOption, setSelectedIAOption] = useState(null);
+
+  useEffect(() => {
+    setSelectedIAOption(backing);
+  }, []);
 
   const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    console.log(option);
+    setSelectedPlayerOption(option);
   };
 
   const handleJugar = () => {
-    // Lógica para manejar el evento al presionar el botón "Jugar"
-    console.log("Botón 'Jugar' presionado");
-    // Aquí puedes agregar cualquier otra lógica que desees ejecutar al presionar el botón
+    if (selectedPlayerOption !== backing) {
+      console.log("Botón 'Jugar' presionado");
+      const iaOptions = ["Piedra", "Papel", "Tijera", "Lagarto", "Spock"];
+      const randomIndex = Math.floor(Math.random() * iaOptions.length);
+      const iaSelection = iaOptions[randomIndex];
+      setSelectedIAOption(iaSelection);
+
+      // Reiniciar los estados después de 5 segundos
+      setTimeout(() => {
+        setSelectedPlayerOption(backing);
+        setSelectedIAOption(backing);
+      }, 5000);
+    } else {
+      alert("Debes seleccionar una opción antes de jugar");
+    }
   };
 
   const getImageForOption = (option) => {
@@ -34,42 +51,43 @@ const PlayerMenu = () => {
       case "Spock":
         return spockImg;
       default:
-        return null;
+        return backing;
     }
   };
 
-  const selectedOptionImage = getImageForOption(selectedOption);
+  const playerOptionImage = getImageForOption(selectedPlayerOption);
+  const iaOptionImage = selectedIAOption ? getImageForOption(selectedIAOption) : backing;
 
   return (
-    <div className="general-container">
-      <div className="side-menu">
+    <div className="general-container_left">
+      <div className="side-menu-left">
         <div className="buttons-list-menu">
           <button
-            className={`game-menu-item ${selectedOption === "Piedra" && "selected"}`}
+            className={`game-menu-item ${selectedPlayerOption === "Piedra" && "selected"}`}
             onClick={() => handleOptionClick("Piedra")}
           >
             Piedra
           </button>
           <button
-            className={`game-menu-item ${selectedOption === "Papel" && "selected"}`}
+            className={`game-menu-item ${selectedPlayerOption === "Papel" && "selected"}`}
             onClick={() => handleOptionClick("Papel")}
           >
             Papel
           </button>
           <button
-            className={`game-menu-item ${selectedOption === "Tijera" && "selected"}`}
+            className={`game-menu-item ${selectedPlayerOption === "Tijera" && "selected"}`}
             onClick={() => handleOptionClick("Tijera")}
           >
             Tijera
           </button>
           <button
-            className={`game-menu-item ${selectedOption === "Lagarto" && "selected"}`}
+            className={`game-menu-item ${selectedPlayerOption === "Lagarto" && "selected"}`}
             onClick={() => handleOptionClick("Lagarto")}
           >
             Lagarto
           </button>
           <button
-            className={`game-menu-item ${selectedOption === "Spock" && "selected"}`}
+            className={`game-menu-item ${selectedPlayerOption === "Spock" &&"selected"}`}
             onClick={() => handleOptionClick("Spock")}
           >
             Spock
@@ -79,10 +97,22 @@ const PlayerMenu = () => {
         <div className="playground-button">
           <GameButton onClick={handleJugar}>Jugar</GameButton>
         </div>
-        
-        {selectedOption && (
+
+        {selectedPlayerOption && (
           <div className="option-image-container">
-            <img src={selectedOptionImage} alt={selectedOption} className="option-image" />
+            <img src={playerOptionImage} alt={selectedPlayerOption} className="option-image" />
+          </div>
+        )}
+      </div>
+
+      <div className="side-menu-right">
+        <div className="playground-button">
+          <GameButton onClick={handleJugar}>Jugar</GameButton>
+        </div>
+
+        {selectedIAOption && (
+          <div className="option-image-container">
+            <img src={iaOptionImage} alt={selectedIAOption} className="option-image" />
           </div>
         )}
       </div>
