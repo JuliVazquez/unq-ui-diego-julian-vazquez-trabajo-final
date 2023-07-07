d7527636df7503589c8abfdc660c1d2f9d67eec5
JuliVazquez <julivazquez09@gmail.com>
2023-07-06 21:38:07 -0300

    reestructuracion de archivos

--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Launcher from "./components/Pages/Launcher/Launcher";
 import NotFoundPage from "./components/Pages/NotFoundPage/NotFoundPage";
-import Playground from "./components/Pages/Playground/Playground";
+import Playground from "./components/Pages/Playground/Playground"
 
 const App = () => {
   return (
